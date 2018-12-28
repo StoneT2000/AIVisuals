c49ceb6b7470960f67a5d15fed25ee452106fd8e
StoneT2000 <stoneztao@gmail.com>
2018-12-28 21:38:29 +0800

    Made landing page

---
+++ scripts/base.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+  $( '.footer_fascade' ).mouseenter(
+    function() {
+      $('.footer').css('transform','translateY(5px)');
+    }
+  ).mouseleave(function(){
+    $('.footer').css('transform','translateY(60px)');
+  });
+  $('.footer').mouseenter(function() {
+    $('.footer').css('transform','translateY(5px)');
+  })
+  $('.footer').mouseleave(function() {
+      $('.footer').css('transform','translateY(60px)');
+    });
+});
